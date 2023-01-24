2f860b4b27c485052615c1572120399237ed46f9
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-01-24 15:54:18 -0500

    Create Results component to de-clutter SearchParams

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pet";
+import Results from "./Results";
 import useBreedList from "./useBreedList";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
@@ -81,17 +81,9 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
