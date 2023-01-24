7ad42e611911154167f7f00e6a0efbce6a7f36e4
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-01-24 12:43:00 -0500

    setState hook for location in SearchParams

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 const SearchParams = () => {
-  const location = "Oakville, ON";
+  const [location, setLocation] = useState("");
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
         </label>
         <button>Submit</button>
       </form>
