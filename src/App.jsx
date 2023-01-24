61a9e1831977f83753f8cbb9003d0652f6733e95
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-01-24 12:12:35 -0500

    Refactor App component to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Dioji",
-      animal: "Dog",
-      breed: "Doberman",
-    }),
-    React.createElement(Pet, {
-      name: "Siatey",
-      animal: "Cat",
-      breed: "Sphynx",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Dioji" animal="dog" breed="Doberman" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Siatee" animal="cat" breed="Sphynx" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+const root = createRoot(container);
+root.render(<App />);
