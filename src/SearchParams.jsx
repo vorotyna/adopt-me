032c6fb23aa073ad88bd302d1f745ab1263848fe
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-01-24 12:56:00 -0500

    setState for animal, map through animal data with hooks

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
 
   return (
     <div className="search-params">
@@ -14,6 +17,20 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+          >
+            <option>
+              {ANIMALS.map((animal) => (
+                <option key={animal}> {animal}</option>
+              ))}
+            </option>
+          </select>
         </label>
         <button>Submit</button>
       </form>
