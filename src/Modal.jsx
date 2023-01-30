9fbf6700cfff891f50c4bec3cdfa4d0aacfe0650
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-01-29 20:47:33 -0500

    Create Modal with portals

---
+++ src/Modal.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children }) => {
+  const elRef = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    const modalRoot = document.getElementById("modal");
+    modalRoot.appendChild(elRef.current);
+
+    return () => modalRoot.removeChild(elRef.current);
+  }, []);
+
+  // Anything that you return in a useEffect is run after the component is unmounted from the dom
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
