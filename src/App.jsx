691d4544f38d75e52ef4be942ae2d7416e7d4be5
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-01-31 17:44:47 -0500

    Add styling to header

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
           <AdoptedPetContext.Provider value={adoptedPet}>
-            <header>
+            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
               <Link to="/">Adopt Me!</Link>
             </header>
             <Routes>
